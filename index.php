861d86c13da4d4661a8757ea57b2467d7b3e59e6
nischalgrs <nischalgrs@gmail.com>
2025-02-25 20:52:47 -0600

    Bug resolved. New page uploaded by NG

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-ls<?php
-echo "Deploying PHP Based Web Application on Apache Web Server using CI/CD Pipeline. New update Nischal;
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> My first PHP page </h1>
+
+
+<?php
+echo "Deploying PHP Based Web Application on Apache Web Server using CI/CD Pipeline. New update Nischal";
 ?>
